4d7b607e27552e8e623784adde3f97193fa513bc
jukyokim <jukyokim1992@gmail.com>
2015-10-14 17:29:34 -0400

    need to figure out this if else conditional

--- script/app.js
+++ script/app.js
@@ -16,6 +16,14 @@ var Board = function(){
 	this.stacked_deck = [];
 };// end of board factory
 
-Board.prototype.shuffle_deck = function() {
-	
-};
+Board.prototype.create_deck = function() {
+	this.stacked_deck = ["a",2,3,4,5,6,7,8,9,10,"j","q","k"];
+	var randomizer = function(){
+		var rand = Math.floor(Math.random()*10);
+		if(rand === 0 || rand === 9){
+			var other_index_num = Math.floor(Math.random()*10);
+			//create an else if that will grab 10 or higher numbers
+		};// if else for grabbing 10 or higher value
+
+	};// end of randomizer function
+};// end of create deck function
