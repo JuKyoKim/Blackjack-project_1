0f255dc60e5c841ef5d58480a76fdf724fdbe825
jukyokim <jukyokim1992@gmail.com>
2015-10-14 16:00:09 -0400

    Added the jquery tag, and also added the background image as a check for the file loading correctly on to the HTML

--- script/app.js
+++ script/app.js
@@ -1,1 +1,6 @@
 console.log("loaded");
+
+window.onload = function(){
+	//set the background on load of the window
+	$("body").css("background-image","url(http://d2fhka9tf2vaj2.cloudfront.net/premium/034_blackJack/images/9.jpg)");
+}
