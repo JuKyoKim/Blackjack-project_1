3d9924f960b5e285d27587a291b2df7509952930
jukyokim <jukyokim1992@gmail.com>
2015-10-15 12:46:17 -0400

    going to take a break from Javascript, and create HTML stuff real fast

--- script/app.js
+++ script/app.js
@@ -2,7 +2,10 @@ console.log("loaded");
 
 window.onload = function(){
 	//set the background on load of the window
-	$("body").css("background-image","url(http://d2fhka9tf2vaj2.cloudfront.net/premium/034_blackJack/images/9.jpg)");
+	var body = $("body")
+	body.css("background-image","url(http://d2fhka9tf2vaj2.cloudfront.net/premium/034_blackJack/images/9.jpg)");
+	
+
 };// end of window onload 
 
 var Board = function(){
