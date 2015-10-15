f674114494875a1dea7abb920d0a2e8534632d7b
jukyokim <jukyokim1992@gmail.com>
2015-10-15 11:10:06 -0400

    created the randomize method

--- script/app.js
+++ script/app.js
@@ -6,13 +6,26 @@ window.onload = function(){
 };// end of window onload 
 
 var Board = function(){
-	//the dealer and player ill store card objects into
-	this.dealer_hand = [];
+	//the dealer and player will store card objects
 	this.player_hand = [];
-	//the bankroll and bet will be placed on default 0 so that 
-	//it can be modified later within a new declaration of the object
 	this.bankroll = 0;
 	this.player_bet = 0;
-	this.stacked_deck = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"];
-	this.card_suit = ["Hearts","Spades","Clovers","Diamonds"]
 };// end of board factory
+
+Board.prototype.randomize = function() {
+	//fisher yates algorithum, need to make my own iteration of it
+	//http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+	var stacked_deck = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"];
+	var card_suit = ["Hearts","Spades","Clovers","Diamonds"]
+	var random_index = function(array){
+		return Math.floor(Math.random()*array.length);	
+	};
+
+	//created a simple "factory" that will return a random card and push that shit towards the array
+	var Card = {
+		card_value:stacked_deck[random_index(stacked_deck)],
+		card_suit:card_suit[random_index(card_suit)]
+	};//end of card factory (sort of)
+	this.player_hand.push(Card);
+};// end of randomize proto method
+
