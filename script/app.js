8184db5eeae7ba14d1b4ddbd6d245c51e63361cb
jukyokim <jukyokim1992@gmail.com>
2015-10-18 18:12:16 -0400

    added JS, cant find a way to loop it so going to just end it here and apply css

--- script/app.js
+++ script/app.js
@@ -2,7 +2,7 @@ var Board = function(){
 	//the dealer and player will store card objects
 	this.dealer_hand = [];
 	this.player_hand = [];
-	this.bankroll = 100;// gonna start default with 100pesos
+	this.bankroll = 200;// gonna start default with 200pesos
 	this.player_bet = 0;
 };// end of board factory
 
@@ -26,12 +26,12 @@ Board.prototype.randomize = function() {
 };// end of randomize proto method
 
 Board.prototype.ask_bets = function(amount){
-	var staked_pesos = amount;
 	if(this.bankroll < amount){
-		return "INVALID!!!! U GOT NO MONEY!!!!"
+		console.log( "INVALID!!!! U GOT NO MONEY!!!!");
+		return "NO BUENO";
 	};
-	this.player_bet = staked_pesos;
-	this.bankroll -= staked_pesos;
+	this.player_bet = amount;
+	this.bankroll -= amount;
 
 };// end of ask_bets method
 
@@ -44,6 +44,8 @@ Board.prototype.deal_hands = function(){
 	for(var i = 0; i < 2;i++){
 		this.dealer_hand.push(this.randomize());
 		this.player_hand.push(this.randomize());
+		this.generate_card("player");
+		this.generate_card("dealer")
 	};// end of for loop that creates new hands for the player
 };// end of start round method
 
@@ -52,11 +54,12 @@ Board.prototype.hit_stay = function(string) {
 	switch(string){
 		case "hit":
 			this.player_hand.push(this.randomize());
+			this.generate_card("player");
 		break;
 
 		case "stay":
 			console.log("robot's turn");
-			this.robo();
+			return this.robo();
 		break;
 
 		case "dealer"://if dealer string is passed throw that dealer a bone
@@ -127,10 +130,12 @@ Board.prototype.robo = function() {
 	var dealers_current_value = this.grab_value("dealer");//grabs the current value for dealer
 	while(dealers_current_value < 16){
 		this.hit_stay("dealer");
+		this.generate_card("dealer");
 		//reset the value to the current value again
 		dealers_current_value = this.grab_value("dealer");
 	};// end of while loop
-	//add a line to proc the win loss check
+	console.log("robo turn done");
+	return this.find_winner();
 };// end of robo method
 
 Board.prototype.find_winner = function(){
@@ -138,37 +143,76 @@ Board.prototype.find_winner = function(){
 	var dealer = this.grab_value("dealer");
 
 	var loss = function(){
-		this.player_bet = 0;//reset to 0
+		console.log("YOU LOST!");
 		return "YOU LOST!"
 	};// end of loss function
 
 	var win = function(){
-		this.bankroll += (this.player_bet * 2);
-		this.player_bet = 0;//reset to 0
+		console.log("YOU WIN!");
 		return "YOU WIN!"
 	};// end of win function
 
 	switch(player){
 		case 21:
+			this.bankroll += (this.player_bet * 2);
+			this.player_bet = 0;//reset to 0
 			win();
 			return "Blackjack"
 		break;
 	
 		default:
 			if(player > 21){
+				this.player_bet = 0;//reset to 0
 				return loss();
 			}else if(dealer > 21){
+				this.bankroll += (this.player_bet * 2);
+				this.player_bet = 0;//reset to 0
 				return win();
 			}else{
 				if(player <= dealer){
+					this.player_bet = 0;//reset to 0
 					return loss();
 				}else{
+					this.bankroll += (this.player_bet * 2);
+					this.player_bet = 0;//reset to 0
 					return win();
 				};// end of the if dealer or winner has more or less in between 21
 			};// end of the if dealer or winner instant bust from having more than 21
 	};// end of conditional switch for checking for blackjack
 };// end of find_method
 
+Board.prototype.generate_card = function(type){
+
+	// player_card_count
+	// dealer_card_count
+	var add_to_count = null;
+	var hand = null;
+	var place = null;
+	if(type === "dealer"){
+		add_to_count = dealer_card_count;
+		hand = this.dealer_hand;
+		place = $("#dealer_display_area");
+	}else if(type === "player"){
+		add_to_count = player_card_count;
+		hand = this.player_hand;
+		place = $("#player_display_area");
+	};// end of if conditional
+
+
+	var card_div = $("<div>");
+	place.append(card_div);
+	card_div.addClass("playing_card");		
+	card_div.attr("id",type);
+	card_div.attr("suit",hand[add_to_count].card_value);
+	card_div.text(hand[add_to_count].card_value);
+
+	if(type === "dealer"){
+		dealer_card_count += 1;
+	}else if(type === "player"){
+		player_card_count += 1;
+	};// to increment at the end, so it generates the next card
+
+};// end of the generate card method
 
 // randomize() - this generates random cards
 // ask_bets(number) - this will create bets and set the value to the current objecy
@@ -179,6 +223,11 @@ Board.prototype.find_winner = function(){
 // robo() - that robotic dealer logic, super simple
 // find_winner() - accounts for the 22 or more busting instant, blackjack at 21, and the dealer always winning out on tied numbs
 
+//global variables
+var player_card_count = 0;
+var dealer_card_count = 0;
+var game_over = false;
+
 
 $(function() {
 	//write all jquery selectors here
@@ -186,73 +235,81 @@ $(function() {
 	var start_button = $("#start_game");
 	var hit_button = $("#hit");
 	var stay_button = $("#stay");
-	var input = $("input");// only one on the page
+	var input = $("#bet_input");// only one on the page
 	var bets_button = $("#bets");
-	var bankroll_text = $("#bankroll_num_value");
-
+	var overall_container = $("#overall_container");
+	var dealer_display = $("#dealer_display_area");
+	var player_display = $("#player_display_area");
+	var next_button = $("#next_round");
 	console.log("loaded");
 	//set the background on load of the window
 	body.css("background-image","url(http://d2fhka9tf2vaj2.cloudfront.net/premium/034_blackJack/images/9.jpg)");
+	overall_container.toggle();
+	next_button.toggle();
 
+	var test = new Board;// not gonna bother renaming this since it wont really matter
 
 	//need to write the function that will loop this game
 	var new_start_game = function(){
+		start_button.toggle();
+		overall_container.toggle();
 		console.log("starting game");
-		$("#start_game").remove();//removes the button
-		var test = new Board;// not gonna bother renaming this since it wont really matter
-		while(test.bankroll !== 0){
-			test.deal_hands();
-			console.log(test);
-			
-			
-
-
-
-
-
-			test.bankroll = Number(prompt("checking!! add a number"));
-		};//while loop
-		var start_button = $("<button>");
-		start_button.attr("id","start_game");
-		start_button.text("Start Game");
-		body.prepend(start_button);
-		alert("YOU LOST ALL YO MONEY!!!!");
+		hit_button.toggle();
+		stay_button.toggle();
 	};// end of the new start game function
 
-
-
-
-
 	//need to write the onclick here!
-	body.on("click", start_button ,new_start_game);//adds back the start game function once the game is over
+	start_button.on("click", start_button ,new_start_game);//adds back the start game function once the game is over
+
+	bets_button.on("click",bets_button,function(){
+		var bet = Number(input.val());
+		if(bet > test.bankroll){
+			console.log("invalid action");
+			return "Whats up son";
+		};
+		test.ask_bets(bet);
+		input.toggle();
+		bets_button.toggle();
+		hit_button.toggle();
+		stay_button.toggle();
+		test.deal_hands();
+		console.log(test);
+	});
+
+	hit_button.on("click",hit_button,function(){
+		test.hit_stay("hit");
+		console.log(test.player_hand);
+	});
+
+	stay_button.on("click",stay_button,function(){
+		test.hit_stay("stay");
+		console.log(test);
+		next_button.toggle();
+		hit_button.toggle();
+		stay_button.toggle();
+	});
+
+	next_button.on("click",next_button,function(){
+		dealer_display.empty();
+		player_display.empty();
+		test.clear_hand();
+		input.toggle();
+		bets_button.toggle();
+		next_button.toggle();
+		dealer_card_count = 0;
+		player_card_count = 0;
+		if(test.bankroll <= 0){
+			test = new Board;
+			input.toggle();
+			bets_button.toggle();		
+			start_button.toggle();
+		};
+	})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	// $("#start_game").toggle();
+	// 	start_button.toggle();
+	//	overall_container.toggle();¬
 
 
 
